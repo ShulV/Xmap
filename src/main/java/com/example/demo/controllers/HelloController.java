56bc685e7bf3426faf0cfcc8cc3266fec509ff1f
Victor Shulpov (NB) <vshulpov@list.ru>
2023-03-03 15:45:50 +0700

    CI/CD v2

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping()
     public String getHello() {
-        return "Оно работает!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+        return "Оно работает!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! v2";
     }
 }
